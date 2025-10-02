ecc55367940ee632131b19426cf4ce09f45cafee
Amorim_dev <guilhermeamorimrochalima@gmail.com>
2025-10-02 10:39:38 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,12 +1,4 @@
-<!DOCTYPE html>
-<html lang="pt-br">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Index</title>
-</head>
-<body>
-    <?php
+<?php
 
         require_once 'header.php';
 
@@ -14,8 +6,8 @@
 
         require_once 'footer.php'
 
-    ?>
-</body>
-</html>
+?>
+
+
 
 
